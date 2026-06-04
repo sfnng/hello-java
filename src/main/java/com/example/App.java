b6c43736142c3f542dea5a1a201b0906bbd67241
Norman Ng <normanng@blackduck.com>
2026-06-04 22:40:00 +0000

    Initial commit: hello-java with vulnerable deps for Fix PR testing

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+
+public class App {
+
+    private static final Logger log = Logger.getLogger(App.class);
+
+    public static void main(String[] args) throws Exception {
+        log.info("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
